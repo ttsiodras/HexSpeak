018b478258eadd21de8b3b0ffb0d45591cf8d2af
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-05-23 22:02:19 +0200

    Works fine

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-make && java -jar ./target/uberjar/thanassis-0.1.0-SNAPSHOT-standalone.jar 1 2
+#!/bin/bash
+make && java -jar ./target/uberjar/thanassis-0.1.0-SNAPSHOT-standalone.jar "$@"
--- src/thanassis/core.clj
+++ src/thanassis/core.clj
@@ -1,32 +1,35 @@
 (ns thanassis.core
   (:gen-class))
 
-(def targetLength 1)
-
-(defn good-words [rdr]
-  (let [letters "abcdef01"
-        matcher (re-pattern (str "^[" letters "]*$"))]
+(defn good-words [rdr letters]
+  (let [matcher (re-pattern (str "^[" letters "]*$"))]
     (doall (->> (line-seq rdr)
-                ;(map #(.toLowerCase %))
                 (filter #(and (> (.length %) 2)
                               (re-matches matcher %)
                               (not (contains? #{"aaa" "aba" "abc"}  %))))))))
 
-(defn get-words-per-length []
+(defn get-words-per-length [letters]
   (let [input-file "/usr/share/dict/words"
         candidates (with-open [rdr (clojure.java.io/reader input-file)]
-                     (good-words rdr))]
+                     (good-words rdr letters))]
     (group-by #(.length %) (concat candidates ["a"]))))
 
-(defn solve [words-per-length phrase phrase-len used-words]
-  (if (= targetLength phrase-len)
-    (printf "%s %s\n" used-words phrase)
-    (for [i (range 1 (inc (- targetLength phrase-len)))
-          w (get words-per-length i [])
-          :when (not (contains? used-words w))]
-      (solve words-per-length (str phrase w) (+ phrase-len i) (conj used-words w)))))
+(defn solve [words-per-length target-length phrase phrase-len used-words results]
+  (if (= target-length phrase-len)
+    (conj results phrase)
+    (apply concat (for [i (range 1 (inc (- target-length phrase-len)))
+                        w (get words-per-length i [])
+                        :when (not (contains? used-words w))]
+                    (solve words-per-length target-length (str phrase w) (+ phrase-len i) (conj used-words w) results)))))
 
 (defn -main [& args]
-  (do
-    (solve (get-words-per-length) "" 0 #{})
-    (println)))
+  (let [f1 (first args)
+        f2 (second args)
+        phrase-length (if (nil? f1) 4 (Integer. (re-find #"\d+" f1)))
+        letters (if (nil? f2) "abcdef" f2)
+        res (solve (get-words-per-length letters) phrase-length "" 0 #{} [])]
+    (do
+      (if (= (System/getenv "SHOWALL") "1")
+        (doall (map #(printf "%s\n" %) res))
+        (printf "Total: %d\n" (count res)))
+      (flush))))
