cb6b94ae688fe5b5801a781a5ba41745289d7a32
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-05-26 00:22:58 +0200

    Now part of my GitHub repos

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject thanassis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "HexSpeak - playing with Clojure and Python"
+  :url "https://github.com/ttsiodras/HexSpeak"
+  :license {:name "GNU Public License"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :aot [thanassis.hexspeak]
   ; :main ^:skip-aot thanassis.hexspeak
