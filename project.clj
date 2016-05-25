843adf869d3c0d26592abdceffc1915073a97a52
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-05-25 22:11:42 +0200

    Who needs getrusage

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-getrusage "0.0.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :aot [thanassis.core]
   ; :main ^:skip-aot thanassis.core
   :main thanassis.core
