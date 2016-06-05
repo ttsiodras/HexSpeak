59b01ca56e6e7abc3938e0d5af63d3ff6a6b6f07
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-06-05 21:40:18 +0200

    Tweak boxplot appearance

--- contrib/boxplot.py
+++ contrib/boxplot.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python2
 
-import numpy as np 
-import matplotlib as mpl 
-import os
+import numpy as np
+import matplotlib as mpl
 
 ## agg backend is used to create plot as a .png file
 mpl.use('agg')
 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 ## Read data from benchmarking session
-languages = [
 #    "timings.python.txt",
 #    "timings.clojure.txt",
+languages = [
     "timings.pypy.txt",
     "timings.java.txt",
     "timings.cpp.txt",
@@ -25,7 +24,7 @@ data_to_plot_and_filenames = [
     )
     for lang in languages
 ]
-    
+
 # Create a figure instance
 fig = plt.figure(1, figsize=(9, 6))
 
@@ -39,9 +38,9 @@ bp = ax.boxplot([x[1] for x in data_to_plot_and_filenames], patch_artist=True)
 ## Change outline color, fill color and linewidth of the boxes
 for box in bp['boxes']:
     # change outline color
-    box.set( color='#7570b3', linewidth=2)
+    box.set(color='#7570b3', linewidth=2)
     # change fill color
-    box.set( facecolor = '#1b9e77' )
+    box.set(facecolor='#1b9e77')
 
 ## Change color and linewidth of the whiskers
 for whisker in bp['whiskers']:
@@ -60,15 +59,25 @@ for flier in bp['fliers']:
     flier.set(marker='o', color='#e7298a', alpha=0.5)
 
 ## Custom x-axis labels
-ax.set_xticklabels([x[0] for x in data_to_plot_and_filenames])
+ax.set_xticklabels([x[0] for x in data_to_plot_and_filenames],
+                   fontdict={'fontsize': 9})
+for tick in ax.yaxis.get_major_ticks():
+    # tick.label.set_fontsize('x-small')
+    tick.label.set_fontsize(9)
+    # tick.label.set_rotation('vertical')
 
 ## Remove top axes and right axes ticks
 ax.get_xaxis().tick_bottom()
 ax.get_yaxis().tick_left()
 
 # Set axis labels
-ax.set_xlabel("Language used for implementation")
-ax.set_ylabel("Time (in ms) to count HexSpeak(14) phrases")
+latexify = lambda x: "$" + x.replace(' ', '\\ ') + "$"
+ax.set_xlabel(
+    latexify("Language used for implementation"),
+    fontdict={'fontsize': 13})
+ax.set_ylabel(
+    latexify("Time (in ms) to count HexSpeak(14) phrases"),
+    fontdict={'fontsize': 13})
 
 # Save the figure
 fig.savefig('boxplot.png', bbox_inches='tight')
