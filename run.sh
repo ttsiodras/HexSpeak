2c2294cf1f72c5c7f5fe399d987ea73b12749b5f
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-07-12 23:57:53 +0200

    By default, count HexSpeak-14 words

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 make >/dev/shm/err 2>&1 || { cat /dev/shm/err ; rm /dev/shm/err ; exit 1 ; }
-java -jar ./target/uberjar/thanassis-0.1.0-SNAPSHOT-standalone.jar "$@"
+if [ $# -eq 0 ] ; then
+    java -jar ./target/uberjar/thanassis-0.1.0-SNAPSHOT-standalone.jar 14 abcdef contrib/words
+else
+    java -jar ./target/uberjar/thanassis-0.1.0-SNAPSHOT-standalone.jar "$@"
+fi
